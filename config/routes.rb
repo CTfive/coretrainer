3c1fb16037401abe14bff1fad9f71176034023ea
Zach Hewitt <zach@zachs-mbp.home>
2019-10-15 21:49:36 -0600

    added controller for usersmade test user creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
   	sessions: 'users/sessions'
   }
-end
-
+  resources :users
   root 'static_pages#index'
-
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
+  describe 'users#create action' do
+    it 'should create a user' do 
+      FactoryBot.create(:user)
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
