2c3f0ddb08a0d71cee384bbd54d67625a56626d0
Zach Hewitt <zach@zachs-mbp.home>
2019-10-16 15:28:39 -0600

    made biometric factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
   factory :biometric do
-    
-  end
-
-  factory :user do 
     sequence :email do |n|
       "dummbemail#{n}@gmail.com"
     end
     password {"secretPassword"}
     password_confirmation {"secretPassword"}
+  end
+
+  factory :biometric do 
     name {"Jack"}
     phone {"923-032-2323"}
     birthday {12.years.ago}
