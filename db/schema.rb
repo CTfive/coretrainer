7d13adc238e45aa7aafb1cb0de74fa2b454bac3c
Michael <mr.enviro@gmail.com>
2019-10-24 20:17:24 +0000

    fixes role bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,40 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_010941) do
+ActiveRecord::Schema.define(version: 2019_10_24_201353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "admins", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "clients", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.float "height"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "profiles", force: :cascade do |t|
-    t.string "name"
-    t.string "phone"
-    t.integer "age"
-    t.float "height"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "user_id"
-    t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
-  create_table "trainers", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -57,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_10_24_010941) do
     t.boolean "client", default: true
     t.boolean "trainer", default: false
     t.boolean "admin", default: false
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
