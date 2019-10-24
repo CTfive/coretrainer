407c0bc4f37ee8b0b612cfc4cf682dd946776b41
Michael <mr.enviro@gmail.com>
2019-10-24 00:04:05 +0000

    creates user role models and controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+	  resources :admins
+	  resources :trainers
+	  resources :clients
+	  
   root 'static_pages#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_033435) do
+ActiveRecord::Schema.define(version: 2019_10_23_234650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string "name"
@@ -24,6 +36,12 @@ ActiveRecord::Schema.define(version: 2019_10_23_033435) do
     t.datetime "updated_at", null: false
     t.string "user_id"
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "trainers", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
