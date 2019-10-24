08d80d15f777548a0fa6691d05a48554c8c43200
Michael <mr.enviro@gmail.com>
2019-10-24 16:35:45 +0000

    fixes all db.schema tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,40 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_010941) do
+ActiveRecord::Schema.define(version: 2019_10_24_162052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "admins", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "clients", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.float "height"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "profiles", force: :cascade do |t|
-    t.string "name"
-    t.string "phone"
-    t.integer "age"
-    t.float "height"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "user_id"
-    t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
-  create_table "trainers", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
