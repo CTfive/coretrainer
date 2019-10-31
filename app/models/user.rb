b6a5f3e70535de54f88d353f0f644cfd162fb662
Zach Hewitt <zach@zachs-mbp.home>
2019-10-31 00:10:59 -0600

    Each role has its own method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   rolify
-  attr_accessor :input_roles
+  attr_accessor :input_roles, :trainer_role, :client_role, :admin_role
 
   
   #after_initialize :set_default_role, :if => :new_record?
@@ -18,4 +18,15 @@ class User < ApplicationRecord
     self.input_roles.to_a["trainer", "client"]
   end
 
+  def trainer_role
+    self.add_role "trainer"
+  end
+
+  def client_role
+    self.add_role "client"
+  end
+
+  def admin_role
+    self.add_role "trainer"
+  end
 end
