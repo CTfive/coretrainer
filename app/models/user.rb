82f5886fad594344218a6a2acb9d6fa361ca1470
Zach Hewitt <zach@Zachs-MacBook-Pro.local>
2019-11-05 14:42:43 -0700

    added correct associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
-  rolify
   attr_accessor :input_roles, :assign_trainer_role
-  after_create :assign_default_role
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, and :omniauthable
@@ -13,31 +11,4 @@ class User < ApplicationRecord
   # has_many :roles
 
 
-  def assign_default_role
-    if self.roles.blank?
-      self.add_role(:client)
-    end
-  end
-
-  def assign_trainer_role
-    if self.has_role? "client"
-      self.add_role "trainer"
-    end
-  end
-
-  # def input_roles
-  #   self.input_roles.to_a["trainer", "client"]
-  # end
-
-  # def trainer_role
-  #   self.add_role "trainer"
-  # end
-
-  # def client_role
-  #   self.add_role "client"
-  # end
-
-  # def admin_role
-  #   self.add_role "trainer"
-  # end
 end
