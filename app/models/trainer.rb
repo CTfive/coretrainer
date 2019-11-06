3bf2ea0669e8e53076e9f1eac0c75fb397192f1e
Zach Hewitt <zach@Zachs-MacBook-Pro.local>
2019-11-05 21:05:35 -0700

    associations!!

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -2,6 +2,5 @@ class Trainer < ApplicationRecord
 	has_many :appointments
 	has_many :clients, through: :appointments
 	has_many :workouts
-	has_many :workout_logs
-	has_many :users
+	has_many :workoutlogs, through: :appointments
 end
