2d9334a3d6c1f4016cc0448fb7506d97bfa20892
Zach Hewitt <zach@Zachs-MacBook-Pro.local>
2019-11-05 23:09:17 -0700

    uncommented lines

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -28,15 +28,15 @@ class ClientsController < ApplicationController
   def create
     @client = Client.create(client_params)
 
-    # respond_to do |format|
-    #   if @client.save
-    #     format.html { redirect_to @client, notice: 'Client was successfully created.' }
-    #     format.json { render :show, status: :created, location: @client }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @client.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    respond_to do |format|
+      if @client.save
+        format.html { redirect_to @client, notice: 'Client was successfully created.' }
+        format.json { render :show, status: :created, location: @client }
+      else
+        format.html { render :new }
+        format.json { render json: @client.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # PATCH/PUT /clients/1
@@ -44,15 +44,15 @@ class ClientsController < ApplicationController
   def update
   	@client = set_client
   	@client.update(client_params)
-    # respond_to do |format|
-    #   if @client.update(client_params)
-    #     format.html { redirect_to @client, notice: 'Client was successfully updated.' }
-    #     format.json { render :show, status: :ok, location: @client }
-    #   else
-    #     format.html { render :edit }
-    #     format.json { render json: @client.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    respond_to do |format|
+      if @client.update(client_params)
+        format.html { redirect_to @client, notice: 'Client was successfully updated.' }
+        format.json { render :show, status: :ok, location: @client }
+      else
+        format.html { render :edit }
+        format.json { render json: @client.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # DELETE /clients/1
@@ -60,16 +60,16 @@ class ClientsController < ApplicationController
   def destroy
   	@client = set_client
     @client.destroy
-    # respond_to do |format|
-    #   format.html { redirect_to clients_url, notice: 'Client was successfully destroyed.' }
-    #   format.json { head :no_content }
-    # end
+    respond_to do |format|
+      format.html { redirect_to clients_url, notice: 'Client was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_client
-      @client = Client.find(params[:id])
+      @client = current_user.clients.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
