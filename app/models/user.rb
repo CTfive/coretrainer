d4535efe34880648329784a3faef75d8e1f864c4
Zach Hewitt <zach@zachs-mbp.home>
2019-11-13 19:59:56 -0700

    removed confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   has_many :trainers
   has_many :clients
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, 
-         :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
   validates :role, presence: true
 
   enum role: %i[client trainer]
